ad7a8a483f54c0214f8eeaa141d999983900b56d
Seb Ashton <seb@sebashton.com>
2016-02-22 10:40:23 +0000

    Fix install_generator

--- lib/generators/solidus_offsite_payments/install/install_generator.rb
+++ lib/generators/solidus_offsite_payments/install/install_generator.rb
@@ -1,8 +1,8 @@
-module Solidus.
+module SolidusOffsitePayments
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      class_option :auto_run_migrations, :type => :boolean, :default => false
+      class_option :auto_run_migrations, type: :boolean, default: false
 
       def add_javascripts
         append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/solidus_offsite_payments\n"
@@ -10,8 +10,8 @@ module Solidus.
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/solidus_offsite_payments\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/solidus_offsite_payments\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/solidus_offsite_payments\n", before: /\*\//, verbose: true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/solidus_offsite_payments\n", before: /\*\//, verbose: true
       end
 
       def add_migrations
